1d098e4c51f24f7463cbfb4fc411c625806a3485
ZhenyuGuo-lenovo <zhyguo@bjtu.edu.cn>
2020-12-19 10:50:01 +0800

    实现双向循环列表

--- java_ds1_code/src/com/helper/main2.java
+++ java_ds1_code/src/com/helper/main2.java
@@ -1,20 +1,27 @@
 package com.helper;
 
 import com.bjtu.ArrayList;
+import com.bjtu.LinkList_P.CycleDouble_LinkedList;
+import com.bjtu.LinkList_P.CycleLinkedList;
 import com.bjtu.LinkList_P.Double_LinkedList;
 import com.bjtu.LinkList_P.LinkedList;
 import com.bjtu.LinkList_P.LinkedList_with_head;
 import com.bjtu.List;
 
 public class main2 {
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         // ArrayList<Person> arr = new ArrayList<>();
         // LinkedList<Person> arr = new LinkedList<>();
 
         // List<Person> arr = new ArrayList<>();
         // List<Person> arr = new LinkedList<>();
         // List<Person> arr = new LinkedList_with_head<>();
-        Double_LinkedList<Person> arr = new Double_LinkedList<>();
+        List<Person> arr = new CycleLinkedList<>();
+        // List<Person> arr = new CycleDouble_LinkedList<>();
+        // Double_LinkedList<Person> arr = new Double_LinkedList<>();
+
+        arr.add(new Person(1,"Tom"));
+        arr.add(new Person(2,"Jack"));
         
         for(int i = 0; i < 10; i++)
         {
@@ -25,16 +32,19 @@ public class main2 {
         arr.add(null);
         arr.add(null);
 
-        for(int i = 0; i < 5; i++)
+       
+
+        for(int i = 0; i < 3; i++)
         {
             arr.remove(0);
         }
 
-        // 添加最后一个与删除最后一个
         arr.add(arr.size(),new Person(666, "name:666"));
+
+        // 添加最后一个与删除最后一个
+       
         arr.remove(arr.size()-1);
         
-
         // 添加到第一个
         arr.add(0,new Person(666, "name:666"));
         
@@ -45,6 +55,8 @@ public class main2 {
         System.out.println(arr.indexOf(new Person(6, "name:6")));
         System.out.println(arr.indexOf(new Person(3, "name:2")));
         System.out.println(arr.indexOf(null));
+
+        java.util.LinkedList<Integer> arr2;
 
         
 
--- java_ds1_code/src/com/helper/main3.java
+++ java_ds1_code/src/com/helper/main3.java
@@ -1,15 +1,28 @@
 package com.helper;
 
+import com.bjtu.List;
 import com.bjtu.LinkList_P.*;
 
 public class main3 {
-    public static void main3(String[] args) {
+    public static void main(String[] args) {
         // LinkedList<Integer> slist = new LinkedList<>();
 
-        Person p = new Person(15,"Tom");
+        CycleDouble_LinkedList<Person> arr = new CycleDouble_LinkedList<>();
 
-        System.out.println(p);
+        arr.add(new Person(1,"Tom"));
+        arr.add(new Person(2,"Jack"));
 
+        arr.reset();
+        System.out.println(arr);
+
+        arr.remove();
+        System.out.println(arr);
+
+        System.out.println(arr.current);
+        arr.remove();
+        System.out.println(arr);
+
+        System.out.println(arr.current);
 
     }
 }
