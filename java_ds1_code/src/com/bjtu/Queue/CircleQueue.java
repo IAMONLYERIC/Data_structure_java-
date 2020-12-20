3d74fb156e36930b2ee237fbd67efc9c329ee237
ZhenyuGuo-lenovo <zhyguo@bjtu.edu.cn>
2020-12-20 17:15:04 +0800

    使用数组实现循环队列

---
+++ java_ds1_code/src/com/bjtu/Queue/CircleQueue.java
@@ -0,0 +1,74 @@
+package com.bjtu.Queue;
+
+@SuppressWarnings("unchecked")
+public class CircleQueue<E> {
+    private int size;
+    private Object[] elements;
+    private int front; // 存储队列的前端元素下标
+
+    private final int DEFAULT_CAPACITY = 10;
+
+    public CircleQueue() {
+        elements = new Object[DEFAULT_CAPACITY];
+        front = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void enQueue(E element) {
+        ensureCapacity(size + 1);
+
+        elements[(front + size) % elements.length] = element;
+        size++;
+    }
+
+    public E deQueue() {
+        E oldElement = (E) elements[front];
+
+        elements[front] = null;
+        front = (front + 1) % elements.length;
+
+        size--;
+        return oldElement;
+    }
+
+    public E front() {
+
+        return (E) elements[front];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        str.append("capacity = ").append(elements.length).append("  size= ").append(size).append(", [");
+        for (int i = 0; i < elements.length - 1; i++) {
+            str.append(elements[i]).append(",");
+        }
+        if (elements.length - 1 >= 0)
+            str.append(elements[elements.length - 1]);
+        str.append("]");
+
+        return str.toString();
+    }
+
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity < capacity) {
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            Object[] newElements = new Object[newCapacity];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[(front + i) % oldCapacity];
+            }
+            elements = newElements;
+            front = 0;
+            System.out.println(oldCapacity + "  扩容为： " + newCapacity);
+        }
+    }
+}
--- java_ds1_code/src/com/helper/main_for_Queue.java
+++ java_ds1_code/src/com/helper/main_for_Queue.java
@@ -1,5 +1,6 @@
 package com.helper;
 
+import com.bjtu.Queue.CircleQueue;
 import com.bjtu.Queue.Deque;
 import com.bjtu.Queue.Queue;
 
@@ -7,29 +8,43 @@ public class main_for_Queue {
     public static void main(String[] args) {
         // Queue<Integer> queue = new Queue<>();
 
-        // for(int i = 0; i < 10; i ++) {
-        //     queue.enQueue(i);
-        // }
+        CircleQueue<Integer> queue = new CircleQueue<>();
 
-        // System.out.println(queue.front());
-        // System.out.println(queue.size());
-
-        // while(!queue.isEmpty()){
-        //     System.out.println(queue.deQueue());
-        // }
-
-        Deque<Integer> deque = new Deque<>();
-
-        deque.enQueueRear(1);
-        deque.enQueueRear(2);
-        deque.enQueueFront(3);
-        deque.enQueueFront(4);
-
-        // rear 2  1 3 4  head
-        
-        while(!deque.isEmpty()){
-            System.out.println(deque.deQueueFront());
+        for(int i = 0; i < 20; i ++) {
+            queue.enQueue(i);
         }
+
+        System.out.println(queue);
+
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+
+        System.out.println(queue);
+
+        for(int i = 0; i < 3; i ++) {
+            queue.enQueue(i+10);
+        }
+        System.out.println(queue);
+
+        System.out.println(queue.front());
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.deQueue());
+        }
+
+        // Deque<Integer> deque = new Deque<>();
+
+        // deque.enQueueRear(1);
+        // deque.enQueueRear(2);
+        // deque.enQueueFront(3);
+        // deque.enQueueFront(4);
+
+        // // rear 2  1 3 4  head
+        
+        // while(!deque.isEmpty()){
+        //     System.out.println(deque.deQueueFront());
+        // }
         
 
 
