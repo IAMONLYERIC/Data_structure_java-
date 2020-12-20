f0a730f091ef3de5799bcf5ba074516d36793a91
ZhenyuGuo-lenovo <zhyguo@bjtu.edu.cn>
2020-12-20 14:19:55 +0800

    利用双向链表实现双端队列

--- java_ds1_code/src/com/helper/main_for_Queue.java
+++ java_ds1_code/src/com/helper/main_for_Queue.java
@@ -1,21 +1,37 @@
 package com.helper;
 
+import com.bjtu.Queue.Deque;
 import com.bjtu.Queue.Queue;
 
 public class main_for_Queue {
     public static void main(String[] args) {
-        Queue<Integer> queue = new Queue<>();
+        // Queue<Integer> queue = new Queue<>();
 
-        for(int i = 0; i < 10; i ++) {
-            queue.enQueue(i);
+        // for(int i = 0; i < 10; i ++) {
+        //     queue.enQueue(i);
+        // }
+
+        // System.out.println(queue.front());
+        // System.out.println(queue.size());
+
+        // while(!queue.isEmpty()){
+        //     System.out.println(queue.deQueue());
+        // }
+
+        Deque<Integer> deque = new Deque<>();
+
+        deque.enQueueRear(1);
+        deque.enQueueRear(2);
+        deque.enQueueFront(3);
+        deque.enQueueFront(4);
+
+        // rear 2  1 3 4  head
+        
+        while(!deque.isEmpty()){
+            System.out.println(deque.deQueueFront());
         }
+        
 
-        System.out.println(queue.front());
-        System.out.println(queue.size());
-
-        while(!queue.isEmpty()){
-            System.out.println(queue.deQueue());
-        }
 
 
     }
